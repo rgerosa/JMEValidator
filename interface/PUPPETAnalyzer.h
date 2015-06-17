fefd3e8094940315248f4b782a23c1410b61b420
Daniel Abercrombie <dabercro@mit.edu>
2015-06-17 20:56:50 +0200

    Added transverse and longitudinal recoil.

--- interface/PUPPETAnalyzer.h
+++ interface/PUPPETAnalyzer.h
@@ -23,6 +23,9 @@ class PUPPETAnalyzer : public JME::Analyzer
 
     private:
 
+        TVector3 ZVec3;
+        TVector3 RecoilVec3;
+
 	edm::InputTag srcRecoilPFMet_;
 	edm::InputTag srcRecoilPFCHSMet_;
 	edm::InputTag srcRecoilPFPuppiMet_;
@@ -49,58 +52,86 @@ class PUPPETAnalyzer : public JME::Analyzer
         float& recoilPFMet_sumEt_ = tree["recoilPFMet_sumEt"].write<float>();
         float& recoilPFMet_Pt_    = tree["recoilPFMet_Pt"].write<float>();
         float& recoilPFMet_Phi_   = tree["recoilPFMet_Phi"].write<float>();
+        float& recoilPFMet_Perp_   = tree["recoilPFMet_Perp"].write<float>();
+        float& recoilPFMet_Long_   = tree["recoilPFMet_Long"].write<float>();
 
         float& recoilPFMet_uncorrected_sumEt_ = tree["recoilPFMet_uncorrected_sumEt"].write<float>();
         float& recoilPFMet_uncorrected_Pt_    = tree["recoilPFMet_uncorrected_Pt"].write<float>();
         float& recoilPFMet_uncorrected_Phi_   = tree["recoilPFMet_uncorrected_Phi"].write<float>();
+        float& recoilPFMet_uncorrected_Perp_   = tree["recoilPFMet_uncorrected_Perp"].write<float>();
+        float& recoilPFMet_uncorrected_Long_   = tree["recoilPFMet_uncorrected_Long"].write<float>();
 
         float& recoilPFCHSMet_sumEt_ = tree["recoilPFCHSMet_sumEt"].write<float>();
         float& recoilPFCHSMet_Pt_    = tree["recoilPFCHSMet_Pt"].write<float>();
         float& recoilPFCHSMet_Phi_   = tree["recoilPFCHSMet_Phi"].write<float>();
+        float& recoilPFCHSMet_Perp_   = tree["recoilPFCHSMet_Perp"].write<float>();
+        float& recoilPFCHSMet_Long_   = tree["recoilPFCHSMet_Long"].write<float>();
 
         float& recoilPFCHSMet_uncorrected_sumEt_ = tree["recoilPFCHSMet_uncorrected_sumEt"].write<float>();
         float& recoilPFCHSMet_uncorrected_Pt_    = tree["recoilPFCHSMet_uncorrected_Pt"].write<float>();
         float& recoilPFCHSMet_uncorrected_Phi_   = tree["recoilPFCHSMet_uncorrected_Phi"].write<float>();
+        float& recoilPFCHSMet_uncorrected_Perp_   = tree["recoilPFCHSMet_uncorrected_Perp"].write<float>();
+        float& recoilPFCHSMet_uncorrected_Long_   = tree["recoilPFCHSMet_uncorrected_Long"].write<float>();
 
         float& recoilPFPuppiMet_sumEt_ = tree["recoilPFPuppiMet_sumEt"].write<float>();
         float& recoilPFPuppiMet_Pt_    = tree["recoilPFPuppiMet_Pt"].write<float>();
         float& recoilPFPuppiMet_Phi_   = tree["recoilPFPuppiMet_Phi"].write<float>();
+        float& recoilPFPuppiMet_Perp_   = tree["recoilPFPuppiMet_Perp"].write<float>();
+        float& recoilPFPuppiMet_Long_   = tree["recoilPFPuppiMet_Long"].write<float>();
 
         float& recoilPFPuppiMet_uncorrected_sumEt_ = tree["recoilPFPuppiMet_uncorrected_sumEt"].write<float>();
         float& recoilPFPuppiMet_uncorrected_Pt_    = tree["recoilPFPuppiMet_uncorrected_Pt"].write<float>();
         float& recoilPFPuppiMet_uncorrected_Phi_   = tree["recoilPFPuppiMet_uncorrected_Phi"].write<float>();
+        float& recoilPFPuppiMet_uncorrected_Perp_   = tree["recoilPFPuppiMet_uncorrected_Perp"].write<float>();
+        float& recoilPFPuppiMet_uncorrected_Long_   = tree["recoilPFPuppiMet_uncorrected_Long"].write<float>();
 
         float& recoilPFPuppiMet_ChargedPV_sumEt_ = tree["recoilPFPuppiMet_ChargedPV_sumEt"].write<float>();
         float& recoilPFPuppiMet_ChargedPV_Pt_    = tree["recoilPFPuppiMet_ChargedPV_Pt"].write<float>();
         float& recoilPFPuppiMet_ChargedPV_Phi_   = tree["recoilPFPuppiMet_ChargedPV_Phi"].write<float>();
+        float& recoilPFPuppiMet_ChargedPV_Perp_   = tree["recoilPFPuppiMet_ChargedPV_Perp"].write<float>();
+        float& recoilPFPuppiMet_ChargedPV_Long_   = tree["recoilPFPuppiMet_ChargedPV_Long"].write<float>();
 
         float& recoilPFPuppiMet_ChargedPV_uncorrected_sumEt_ = tree["recoilPFPuppiMet_ChargedPV_uncorrected_sumEt"].write<float>();
         float& recoilPFPuppiMet_ChargedPV_uncorrected_Pt_    = tree["recoilPFPuppiMet_ChargedPV_uncorrected_Pt"].write<float>();
         float& recoilPFPuppiMet_ChargedPV_uncorrected_Phi_   = tree["recoilPFPuppiMet_ChargedPV_uncorrected_Phi"].write<float>();
+        float& recoilPFPuppiMet_ChargedPV_uncorrected_Perp_   = tree["recoilPFPuppiMet_ChargedPV_uncorrected_Perp"].write<float>();
+        float& recoilPFPuppiMet_ChargedPV_uncorrected_Long_   = tree["recoilPFPuppiMet_ChargedPV_uncorrected_Long"].write<float>();
 
         float& recoilPFPuppiMet_ChargedPU_sumEt_ = tree["recoilPFPuppiMet_ChargedPU_sumEt"].write<float>();
         float& recoilPFPuppiMet_ChargedPU_Pt_    = tree["recoilPFPuppiMet_ChargedPU_Pt"].write<float>();
         float& recoilPFPuppiMet_ChargedPU_Phi_   = tree["recoilPFPuppiMet_ChargedPU_Phi"].write<float>();
+        float& recoilPFPuppiMet_ChargedPU_Perp_   = tree["recoilPFPuppiMet_ChargedPU_Perp"].write<float>();
+        float& recoilPFPuppiMet_ChargedPU_Long_   = tree["recoilPFPuppiMet_ChargedPU_Long"].write<float>();
 
         float& recoilPFPuppiMet_ChargedPU_uncorrected_sumEt_ = tree["recoilPFPuppiMet_ChargedPU_uncorrected_sumEt"].write<float>();
         float& recoilPFPuppiMet_ChargedPU_uncorrected_Pt_    = tree["recoilPFPuppiMet_ChargedPU_uncorrected_Pt"].write<float>();
         float& recoilPFPuppiMet_ChargedPU_uncorrected_Phi_   = tree["recoilPFPuppiMet_ChargedPU_uncorrected_Phi"].write<float>();
+        float& recoilPFPuppiMet_ChargedPU_uncorrected_Perp_   = tree["recoilPFPuppiMet_ChargedPU_uncorrected_Perp"].write<float>();
+        float& recoilPFPuppiMet_ChargedPU_uncorrected_Long_   = tree["recoilPFPuppiMet_ChargedPU_uncorrected_Long"].write<float>();
 
         float& recoilPFPuppiMet_NeutralPV_sumEt_ = tree["recoilPFPuppiMet_NeutralPV_sumEt"].write<float>();
         float& recoilPFPuppiMet_NeutralPV_Pt_    = tree["recoilPFPuppiMet_NeutralPV_Pt"].write<float>();
         float& recoilPFPuppiMet_NeutralPV_Phi_   = tree["recoilPFPuppiMet_NeutralPV_Phi"].write<float>();
+        float& recoilPFPuppiMet_NeutralPV_Perp_   = tree["recoilPFPuppiMet_NeutralPV_Perp"].write<float>();
+        float& recoilPFPuppiMet_NeutralPV_Long_   = tree["recoilPFPuppiMet_NeutralPV_Long"].write<float>();
 
         float& recoilPFPuppiMet_NeutralPV_uncorrected_sumEt_ = tree["recoilPFPuppiMet_NeutralPV_uncorrected_sumEt"].write<float>();
         float& recoilPFPuppiMet_NeutralPV_uncorrected_Pt_    = tree["recoilPFPuppiMet_NeutralPV_uncorrected_Pt"].write<float>();
         float& recoilPFPuppiMet_NeutralPV_uncorrected_Phi_   = tree["recoilPFPuppiMet_NeutralPV_uncorrected_Phi"].write<float>();
+        float& recoilPFPuppiMet_NeutralPV_uncorrected_Perp_   = tree["recoilPFPuppiMet_NeutralPV_uncorrected_Perp"].write<float>();
+        float& recoilPFPuppiMet_NeutralPV_uncorrected_Long_   = tree["recoilPFPuppiMet_NeutralPV_uncorrected_Long"].write<float>();
 
         float& recoilPFPuppiMet_NeutralPU_sumEt_ = tree["recoilPFPuppiMet_NeutralPU_sumEt"].write<float>();
         float& recoilPFPuppiMet_NeutralPU_Pt_    = tree["recoilPFPuppiMet_NeutralPU_Pt"].write<float>();
         float& recoilPFPuppiMet_NeutralPU_Phi_   = tree["recoilPFPuppiMet_NeutralPU_Phi"].write<float>();
+        float& recoilPFPuppiMet_NeutralPU_Perp_   = tree["recoilPFPuppiMet_NeutralPU_Perp"].write<float>();
+        float& recoilPFPuppiMet_NeutralPU_Long_   = tree["recoilPFPuppiMet_NeutralPU_Long"].write<float>();
 
         float& recoilPFPuppiMet_NeutralPU_uncorrected_sumEt_ = tree["recoilPFPuppiMet_NeutralPU_uncorrected_sumEt"].write<float>();
         float& recoilPFPuppiMet_NeutralPU_uncorrected_Pt_    = tree["recoilPFPuppiMet_NeutralPU_uncorrected_Pt"].write<float>();
         float& recoilPFPuppiMet_NeutralPU_uncorrected_Phi_   = tree["recoilPFPuppiMet_NeutralPU_uncorrected_Phi"].write<float>();
+        float& recoilPFPuppiMet_NeutralPU_uncorrected_Perp_   = tree["recoilPFPuppiMet_NeutralPU_uncorrected_Perp"].write<float>();
+        float& recoilPFPuppiMet_NeutralPU_uncorrected_Long_   = tree["recoilPFPuppiMet_NeutralPU_uncorrected_Long"].write<float>();
 
 	int& NCleanedJets_ =  tree["NCleanedJets"].write<int>();
 	int& NVertex_      =  tree["NVertex"].write<int>();
--- src/PUPPETAnalyzer.cc
+++ src/PUPPETAnalyzer.cc
@@ -150,20 +150,26 @@ void PUPPETAnalyzer::analyze(const edm::Event& iEvent,
   Zboson_Phi_ = Zboson.phi();
   ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<float>> p4(Zboson.pt(), Zboson.eta(), Zboson.phi(), Zboson.energy());
   Zboson_M_   = p4.M();
+  ZVec3.SetPtEtaPhi(Zboson_Pt_,0.0,Zboson_Phi_);
 
   // Recoils
   edm::Handle<std::vector<pat::MET>> RecoilPFMetHandle;
   iEvent.getByToken(srcRecoilPFMetToken_, RecoilPFMetHandle);
 
-
   const pat::MET& metPF = RecoilPFMetHandle->at(0);
   recoilPFMet_sumEt_ = metPF.sumEt();
   recoilPFMet_Pt_    = metPF.pt();
   recoilPFMet_Phi_   = metPF.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFMet_Pt_,0.0,recoilPFMet_Phi_);
+  recoilPFMet_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFMet_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   recoilPFMet_uncorrected_sumEt_ = metPF.uncorrectedSumEt();
   recoilPFMet_uncorrected_Pt_    = metPF.uncorrectedPt();
   recoilPFMet_uncorrected_Phi_   = metPF.uncorrectedPhi();
+  RecoilVec3.SetPtEtaPhi(recoilPFMet_uncorrected_Pt_,0.0,recoilPFMet_uncorrected_Phi_);
+  recoilPFMet_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFMet_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   edm::Handle<std::vector<pat::MET>> RecoilPFMetCHSHandle;
   iEvent.getByToken(srcRecoilPFCHSMetToken_, RecoilPFMetCHSHandle);
@@ -173,24 +179,35 @@ void PUPPETAnalyzer::analyze(const edm::Event& iEvent,
   recoilPFCHSMet_sumEt_ = metPFCHS.sumEt();
   recoilPFCHSMet_Pt_    = metPFCHS.pt();
   recoilPFCHSMet_Phi_   = metPFCHS.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFCHSMet_Pt_,0.0,recoilPFCHSMet_Phi_);
+  recoilPFCHSMet_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFCHSMet_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   recoilPFCHSMet_uncorrected_sumEt_ = metPFCHS.uncorrectedSumEt();
   recoilPFCHSMet_uncorrected_Pt_    = metPFCHS.uncorrectedPt();
   recoilPFCHSMet_uncorrected_Phi_   = metPFCHS.uncorrectedPhi();
+  RecoilVec3.SetPtEtaPhi(recoilPFCHSMet_uncorrected_Pt_,0.0,recoilPFCHSMet_uncorrected_Phi_);
+  recoilPFCHSMet_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFCHSMet_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   edm::Handle<std::vector<pat::MET>> RecoilPFMetPuppiHandle;
   iEvent.getByToken(srcRecoilPFPuppiMetToken_, RecoilPFMetPuppiHandle);
-
 
   const pat::MET& metPFPuppi = RecoilPFMetPuppiHandle->at(0);
   recoilPFPuppiMet_sumEt_ = metPFPuppi.sumEt();
   recoilPFPuppiMet_Pt_    = metPFPuppi.pt();
   recoilPFPuppiMet_Phi_   = metPFPuppi.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_Pt_,0.0,recoilPFPuppiMet_Phi_);
+  recoilPFPuppiMet_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_Long_  = RecoilVec3.Dot(ZVec3.Unit());
+
 
   recoilPFPuppiMet_uncorrected_sumEt_ = metPFPuppi.uncorrectedSumEt();
   recoilPFPuppiMet_uncorrected_Pt_    = metPFPuppi.uncorrectedPt();
   recoilPFPuppiMet_uncorrected_Phi_   = metPFPuppi.uncorrectedPhi();
-
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_uncorrected_Pt_,0.0,recoilPFPuppiMet_uncorrected_Phi_);
+  recoilPFPuppiMet_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   edm::Handle<std::vector<pat::MET>> RecoilPFMetPuppi_ChargedPVHandle;
   iEvent.getByToken(srcRecoilPFPuppiMet_ChargedPVToken_, RecoilPFMetPuppi_ChargedPVHandle);
@@ -199,12 +216,16 @@ void PUPPETAnalyzer::analyze(const edm::Event& iEvent,
   recoilPFPuppiMet_ChargedPV_sumEt_ = metPFPuppi_ChargedPV.sumEt();
   recoilPFPuppiMet_ChargedPV_Pt_    = metPFPuppi_ChargedPV.pt();
   recoilPFPuppiMet_ChargedPV_Phi_   = metPFPuppi_ChargedPV.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_ChargedPV_Pt_,0.0,recoilPFPuppiMet_ChargedPV_Phi_);
+  recoilPFPuppiMet_ChargedPV_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_ChargedPV_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   recoilPFPuppiMet_ChargedPV_uncorrected_sumEt_ = metPFPuppi_ChargedPV.uncorrectedSumEt();
   recoilPFPuppiMet_ChargedPV_uncorrected_Pt_    = metPFPuppi_ChargedPV.uncorrectedPt();
   recoilPFPuppiMet_ChargedPV_uncorrected_Phi_   = metPFPuppi_ChargedPV.uncorrectedPhi();
-
-
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_ChargedPV_uncorrected_Pt_,0.0,recoilPFPuppiMet_ChargedPV_uncorrected_Phi_);
+  recoilPFPuppiMet_ChargedPV_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_ChargedPV_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   edm::Handle<std::vector<pat::MET>> RecoilPFMetPuppi_ChargedPUHandle;
   iEvent.getByToken(srcRecoilPFPuppiMet_ChargedPUToken_, RecoilPFMetPuppi_ChargedPUHandle);
@@ -213,10 +234,16 @@ void PUPPETAnalyzer::analyze(const edm::Event& iEvent,
   recoilPFPuppiMet_ChargedPU_sumEt_ = metPFPuppi_ChargedPU.sumEt();
   recoilPFPuppiMet_ChargedPU_Pt_    = metPFPuppi_ChargedPU.pt();
   recoilPFPuppiMet_ChargedPU_Phi_   = metPFPuppi_ChargedPU.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_ChargedPU_Pt_,0.0,recoilPFPuppiMet_ChargedPU_Phi_);
+  recoilPFPuppiMet_ChargedPU_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_ChargedPU_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   recoilPFPuppiMet_ChargedPU_uncorrected_sumEt_ = metPFPuppi_ChargedPU.uncorrectedSumEt();
   recoilPFPuppiMet_ChargedPU_uncorrected_Pt_    = metPFPuppi_ChargedPU.uncorrectedPt();
   recoilPFPuppiMet_ChargedPU_uncorrected_Phi_   = metPFPuppi_ChargedPU.uncorrectedPhi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_ChargedPU_uncorrected_Pt_,0.0,recoilPFPuppiMet_ChargedPU_uncorrected_Phi_);
+  recoilPFPuppiMet_ChargedPU_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_ChargedPU_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   edm::Handle<std::vector<pat::MET>> RecoilPFMetPuppi_NeutralPVHandle;
   iEvent.getByToken(srcRecoilPFPuppiMet_NeutralPVToken_, RecoilPFMetPuppi_NeutralPVHandle);
@@ -225,12 +252,16 @@ void PUPPETAnalyzer::analyze(const edm::Event& iEvent,
   recoilPFPuppiMet_NeutralPV_sumEt_ = metPFPuppi_NeutralPV.sumEt();
   recoilPFPuppiMet_NeutralPV_Pt_    = metPFPuppi_NeutralPV.pt();
   recoilPFPuppiMet_NeutralPV_Phi_   = metPFPuppi_NeutralPV.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_NeutralPV_Pt_,0.0,recoilPFPuppiMet_NeutralPV_Phi_);
+  recoilPFPuppiMet_NeutralPV_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_NeutralPV_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   recoilPFPuppiMet_NeutralPV_uncorrected_sumEt_ = metPFPuppi_NeutralPV.uncorrectedSumEt();
   recoilPFPuppiMet_NeutralPV_uncorrected_Pt_    = metPFPuppi_NeutralPV.uncorrectedPt();
   recoilPFPuppiMet_NeutralPV_uncorrected_Phi_   = metPFPuppi_NeutralPV.uncorrectedPhi();
-
-
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_NeutralPV_uncorrected_Pt_,0.0,recoilPFPuppiMet_NeutralPV_uncorrected_Phi_);
+  recoilPFPuppiMet_NeutralPV_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_NeutralPV_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   edm::Handle<std::vector<pat::MET>> RecoilPFMetPuppi_NeutralPUHandle;
   iEvent.getByToken(srcRecoilPFPuppiMet_NeutralPUToken_, RecoilPFMetPuppi_NeutralPUHandle);
@@ -239,10 +270,16 @@ void PUPPETAnalyzer::analyze(const edm::Event& iEvent,
   recoilPFPuppiMet_NeutralPU_sumEt_ = metPFPuppi_NeutralPU.sumEt();
   recoilPFPuppiMet_NeutralPU_Pt_    = metPFPuppi_NeutralPU.pt();
   recoilPFPuppiMet_NeutralPU_Phi_   = metPFPuppi_NeutralPU.phi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_NeutralPU_Pt_,0.0,recoilPFPuppiMet_NeutralPU_Phi_);
+  recoilPFPuppiMet_NeutralPU_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_NeutralPU_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   recoilPFPuppiMet_NeutralPU_uncorrected_sumEt_ = metPFPuppi_NeutralPU.uncorrectedSumEt();
   recoilPFPuppiMet_NeutralPU_uncorrected_Pt_    = metPFPuppi_NeutralPU.uncorrectedPt();
   recoilPFPuppiMet_NeutralPU_uncorrected_Phi_   = metPFPuppi_NeutralPU.uncorrectedPhi();
+  RecoilVec3.SetPtEtaPhi(recoilPFPuppiMet_NeutralPU_uncorrected_Pt_,0.0,recoilPFPuppiMet_NeutralPU_uncorrected_Phi_);
+  recoilPFPuppiMet_NeutralPU_uncorrected_Perp_  = RecoilVec3.Perp(ZVec3);
+  recoilPFPuppiMet_NeutralPU_uncorrected_Long_  = RecoilVec3.Dot(ZVec3.Unit());
 
   tree.fill();
   
