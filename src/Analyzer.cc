f485e6870186ff4e6aa6a85757259db45d09c9db
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-07-03 19:58:19 +0200

    Reduce memory usage

--- src/Analyzer.cc
+++ src/Analyzer.cc
@@ -5,6 +5,8 @@
 
 #include <TTree.h>
 
+#include <limits>
+
 JME::Analyzer::Analyzer(const edm::ParameterSet& iConfig)
     : moduleLabel_(iConfig.getParameter<std::string>("@module_label")) {
 
@@ -25,5 +27,15 @@ void JME::Analyzer::beginJob()
         throw edm::Exception(edm::errors::Configuration, "TFileService missing from configuration!");
 
     TTree* tree_ = fs->make<TTree>(treeName_.c_str(), treeName_.c_str());
+
+    tree_->SetDirectory(&fs->file());
+
+    // Disable autosave
+    tree_->SetAutoSave(std::numeric_limits<Long64_t>::max());
+
+    // Debug auto flush is 30 MB. In full mode we have about 32 trees in the output fill, so 960 MB only for buffering.
+    // Reduce to 1 MB per tree to reduce memory footprint
+    tree_->SetAutoFlush(-1 * 1000 * 1000);
+
     tree.init(tree_);
 }
