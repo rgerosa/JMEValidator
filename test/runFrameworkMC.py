7e40c15073baf378309a0ed936309d2d2a90efcf
Raffaele <raffaele.gerosa@cern.ch>
2015-07-04 11:13:28 +0200

    updates

--- test/runFrameworkMC.py
+++ test/runFrameworkMC.py
@@ -27,13 +27,13 @@ process = createProcess(options.isMC, options.globalTag, options.muonTypeID, opt
 if len(options.inputFiles) == 0 and options.isMC == True:
 
      #options.inputFiles.append('root://xrootd-cms.infn.it//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/AsymptNoPURawReco_MCRUN2_74_V9A-v3/10000/263601E1-AB15-E511-B132-3417EBE4E882.root');
-      options.inputFiles.append('root://xrootd.unl.edu//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/StartupFlat10to50bx50Raw_MCRUN2_74_V8-v1/10000/38D1C54C-0F02-E511-A54E-AC853D9F5256.root')
+     #options.inputFiles.append('root://xrootd.unl.edu//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/StartupFlat10to50bx50Raw_MCRUN2_74_V8-v1/10000/38D1C54C-0F02-E511-A54E-AC853D9F5256.root')
      #options.inputFiles.append('/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/02DE3B74-6C08-E511-ABE3-0025905A60D0.root')
 #     options.inputFiles.append('root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/AsymptNoPUbx25Reco_MCRUN2_74_V9-v3/00000/02FAF8EE-3608-E511-AFC8-0025905A612C.root')
       #options.inputFiles.append('root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/AsymptFlat0to50bx50Reco_MCRUN2_74_V9A-v3/00000/023F427F-0E08-E511-A813-0025905A60EE.root')
      #options.inputFiles.append('root://xrootd.unl.edu//store/mc/RunIISpring15DR74/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/AsymptFlat0to50bx25Reco_MCRUN2_74_V9-v3/10000/0031CCC7-B007-E511-A963-0025905964CC.root')
-
-
+      options.inputFiles.append('/store/relval/CMSSW_7_4_4/RelValZMM_13/MINIAODSIM/PU25ns_MCRUN2_74_V9_38Tbis-v1/00000/BAD8497B-5D09-E511-9C05-0025905B8562.root')
+      #options.inputFiles.append('/store/relval/CMSSW_7_4_4/RelValZEE_13/MINIAODSIM/PU25ns_MCRUN2_74_V9_38Tbis-v1/00000/D4F6F957-4809-E511-AD3B-00261894386F.root')    
 
 ## set input files
 process.source.fileNames = cms.untracked.vstring(options.inputFiles);
