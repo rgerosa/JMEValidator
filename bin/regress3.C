3f0b4c5566592212a716ded3c440535d45736492
rfriese <raphael.friese@cern.ch>
2015-07-09 04:14:30 +0200

    preparing trianing macro for 2D training

--- bin/regress3.C
+++ bin/regress3.C
@@ -2,7 +2,6 @@
 #include "TTree.h"
 #include "../interface/GBRTrainer.h"
 #include "CondFormats/EgammaObjects/interface/GBRForest.h"
-//#include "Cintex/Cintex.h"
 
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
@@ -22,6 +21,7 @@ using namespace std;
 void doTraining(boost::property_tree::ptree &pt, TTree* lRegress)
 {
 
+  // adding friends. Important for trainings with several stages
   std::vector<std::string> *friends = new std::vector<std::string>;
   BOOST_FOREACH(boost::property_tree::ptree::value_type &v, pt.get_child("friends"))
   {
@@ -31,15 +31,12 @@ void doTraining(boost::property_tree::ptree &pt, TTree* lRegress)
   for (int i=0; i<int(friends->size()); ++i) {
     lRegress->AddFriend(friends->at(i).c_str(), (friends->at(i)+".root").c_str());
   }
-  std::string weight = pt.get<std::string>("weight");
-  GBRTrainer *train = new GBRTrainer;
-  train->AddTree(lRegress);
-  train->SetTrainingCut(weight);
-  train->SetMinEvents( pt.get<int>("minEvents") );
-  train->SetShrinkage( pt.get<float>("shrinkage") );
-  train->SetMinCutSignificance( pt.get<float>("minCutSignificance") );  
-  cout << " ===> " << weight << endl;  
-  train->SetTargetVar( pt.get<std::string>("targetVariable") );
+  std::vector<std::string> *targetVariables = new std::vector<std::string>;
+  BOOST_FOREACH(boost::property_tree::ptree::value_type &v, pt.get_child("targetVariables"))
+  {
+    assert(v.first.empty());
+    targetVariables->push_back( v.second.data() );
+  }
 
   std::vector<std::string> *lVec = new std::vector<std::string>;
   BOOST_FOREACH(boost::property_tree::ptree::value_type &v, pt.get_child("trainingVariables"))
@@ -48,19 +45,41 @@ void doTraining(boost::property_tree::ptree &pt, TTree* lRegress)
     lVec->push_back( v.second.data() );
   }
 
-  for (int i=0; i<int(lVec->size()); ++i) {
-     train->AddInputVar(lVec->at(i));
+  std::string weight = pt.get<std::string>("weight");
+
+  if( targetVariables->size() == 1)
+  {
+    // 1-dim training
+    GBRTrainer *train = new GBRTrainer;
+    train->AddTree(lRegress);
+    train->SetTrainingCut(weight);
+    train->SetMinEvents( pt.get<int>("minEvents") );
+    train->SetShrinkage( pt.get<float>("shrinkage") );
+    train->SetMinCutSignificance( pt.get<float>("minCutSignificance") );  
+    cout << " ===> " << weight << endl;  
+    train->SetTargetVar( pt.get<std::string>("targetVariable") );
+    for (int i=0; i<int(lVec->size()); ++i) {
+       train->AddInputVar(lVec->at(i));
+    }
+    std::cout << "Training Forest with " << pt.get<int>("nTrees")<< " Trees" << std::endl; 
+    std::cout << pt.get<std::string>("desc") << std::endl;
+    const GBRForest *forest = train->TrainForest( pt.get<int>("nTrees"));
+    //ROOT::Cintex::Cintex::Enable();
+    TFile *fout = new TFile(pt.get<std::string>("weightfilename").c_str(),"RECREATE");    
+    std::string mvaResponseName = pt.get<std::string>("name");
+    fout->WriteObject(lVec, "varlist");
+    fout->WriteObject(forest, mvaResponseName.c_str());
+    fout->Close();
   }
+  else if (targetVariables->size() == 2)
+  {
+   // 2-dim training
+  } 
+
+
+
+
   
-  std::cout << "Training Forest with " << pt.get<int>("nTrees")<< " Trees" << std::endl; 
-  std::cout << pt.get<std::string>("desc") << std::endl;
-  const GBRForest *forest = train->TrainForest( pt.get<int>("nTrees"));
-  //ROOT::Cintex::Cintex::Enable();
-  TFile *fout = new TFile(pt.get<std::string>("weightfilename").c_str(),"RECREATE");    
-  std::string mvaResponseName = pt.get<std::string>("name");
-  fout->WriteObject(lVec, "varlist");
-  fout->WriteObject(forest, mvaResponseName.c_str());
-  fout->Close();
 }
 
 
@@ -125,10 +144,4 @@ int main(int argc, char* argv[] ) {
     }
     inputTree->AddFriend(friendTreename.c_str(), friendFilename.c_str());
   }
-  //merge inputTree and its friends to one file
-  //std::string outputFilename = "output.root";
-  //TFile *outputFile = TFile::Open(outputFilename.c_str(), "RECREATE");
-  //TTree *outputTree = inputTree->CloneTree();
-  //outputTree->Write();
-  //outputFile->Close();
 }
